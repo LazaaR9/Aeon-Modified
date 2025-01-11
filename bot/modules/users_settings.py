7dd3f7ec9a786895535d584b37a974f48c943f86
LazaaR <Githeroku@outlook.com>
2025-01-11 11:10:18 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -261,7 +261,7 @@ async def update_user_settings(
     user_id = query.from_user.id
     thumbnail = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(thumbnail):
-        thumbnail = "https://graph.org/JetMirror-07-24-2"
+        thumbnail = "https://telegra.ph/file/5c4635e173e7407694a63.jpg"
     await edit_message(query.message, msg, button, thumbnail)
 
 
@@ -271,7 +271,7 @@ async def user_settings(_, message):
     user_id = message.from_user.id
     thumbnail = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(thumbnail):
-        thumbnail = "https://graph.org/JetMirror-07-24-2"
+        thumbnail = "https://telegra.ph/file/5c4635e173e7407694a63.jpg"
     x = await send_message(message, msg, button, thumbnail)
     await five_minute_del(message)
     await delete_message(x)
