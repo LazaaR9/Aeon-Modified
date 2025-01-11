e648732dcbe43ab269375aac5c5b4e99865a381e
LazaaR <Githeroku@outlook.com>
2025-01-11 11:07:22 +0530

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -41,7 +41,6 @@ async def mirror_status(_, message):
     if count == 0:
         current_time = get_readable_time(time() - bot_start_time)
         free = get_readable_file_size(disk_usage("/usr/src/app/downloads/").free)
-        msg = f"<b><a href='https://t.me/JetMirror'>Pᴏᴡᴇʀᴇᴅ ʙʏ ᴊᴇᴛ-ᴍɪʀʀᴏʀ 🚀♥️</a></b>\n\n"
         msg += "No downloads are currently in progress.\n"
         msg += f"\n<b>• Bot uptime</b>: {current_time}"
         msg += f"\n<b>• Free disk space</b>: {free}"
