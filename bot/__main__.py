6c63b7fcd572c75478c8c83e9ef042cb67c31da5
LazaaR <Githeroku@outlook.com>
2025-01-11 11:06:14 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -123,7 +123,6 @@ async def stats(_, message):
         '👤 Usᴇʀ ᴛᴀsᴋ': config_dict.get('USER_MAX_TASKS', '∞')
     }
     system_info = (
-        f'<b><a href="https://t.me/JetMirror">Pᴏᴡᴇʀᴇᴅ ʙʏ ᴊᴇᴛ-ᴍɪʀʀᴏʀ 🚀♥️</a></b>\n\n'\
         f'<b>Sʏsᴛᴇᴍ sᴛᴀᴛs 🚀♥️</b>\n\n'\
         f'🤖 Bᴏᴛ ᴜᴘᴛɪᴍᴇ : {current_time}\n'\
         f'🖥️ Sʏs ᴜᴘᴛɪᴍᴇ : {os_uptime}\n'\
