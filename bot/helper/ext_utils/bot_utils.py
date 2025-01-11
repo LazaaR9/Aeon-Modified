485d45089dac198221992a243f6679d8ffdfb072
LazaaR <Githeroku@outlook.com>
2025-01-11 11:20:05 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -109,18 +109,18 @@ STATUS_LIMIT = 4
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading 📤"
-    STATUS_DOWNLOADING = "Downloading 📥"
-    STATUS_CLONING = "Cloning 🔃"
-    STATUS_QUEUEDL = "DL queued ⏳"
-    STATUS_QUEUEUP = "UL queued ⏳"
-    STATUS_PAUSED = "Paused ⛔️"
-    STATUS_ARCHIVING = "Archiving 🛠"
-    STATUS_EXTRACTING = "Extracting 📂"
-    STATUS_SPLITTING = "Splitting ✂️"
-    STATUS_CHECKING = "CheckUp ⏱"
-    STATUS_SEEDING = "Seeding 🌧"
-    STATUS_PROCESSING = "Processing ♻️"
+    STATUS_UPLOADING   = "📤 Uᴘʟᴏᴀᴅɪɴɢ"
+    STATUS_DOWNLOADING = "📥 Dᴏᴡɴʟᴏᴀᴅɪɴɢ"
+    STATUS_CLONING     = "♻️ Cʟᴏɴᴇ"
+    STATUS_QUEUEDL     = "💤 QᴜᴇᴜᴇDL"
+    STATUS_QUEUEUP     = "💤 QᴜᴇᴜᴇUᴘ"
+    STATUS_PAUSED      = "⛔️ Pᴀᴜsᴇ"
+    STATUS_ARCHIVING   = "🔐 Aʀᴄʜɪᴠᴇ"
+    STATUS_EXTRACTING  = "📂 Exᴛʀᴀᴄᴛ"
+    STATUS_SPLITTING   = "✂️ Sᴘʟɪᴛ"
+    STATUS_CHECKING    = "📝 CʜᴇᴄᴋUᴘ"
+    STATUS_SEEDING     = "🌧 Sᴇᴇᴅ"
+    STATUS_PROCESSING  = "⚙️ Pʀᴏᴄᴇssɪɴɢ"
 
 
 class SetInterval:
@@ -237,8 +237,8 @@ def progress_bar(pct):
         100
     )
     cFull = int(p // 10)
-    p_str = "★" * cFull
-    p_str += "✩" * (10 - cFull)
+    p_str = "█" * c_full
+    p_str += "▒" * (10 - c_full)
     return f"{p_str}"
 
 
@@ -251,7 +251,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b><a href="https://t.me/JetMirror">ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴊᴇᴛ-ᴍɪʀʀᴏʀ ❤️🚀</a></b>\n'
+    msg = "<b>MLTB Lite by Aeon</b>\n\n"
     button = None
     tasks = len(download_dict)
     current_time = get_readable_time(time() - bot_start_time)
@@ -272,38 +272,61 @@ def get_readable_message():
             msg += f"\n<pre language='FileName'>{download.name()}</pre>\n"
             msg += f"<b>{download.status()}:</b>"
             # msg += f"<b>\n⌑ ғɪʟᴇɴᴀᴍᴇ</b> » <i>{escape(f'{download.name()}')}</i>\n"
-            msg += f"\n✅ {progress_bar(download.progress())} » {download.progress()}"
-            msg += f"\n<code>💯 Done</code>: {download.processed_bytes()} of {download.size()}"
-            msg += f"\n<code>🚀 Speed</code>: {download.speed()}"
-            msg += f"\n<code>🤖 Engine</code>: <b>{download.engine}</b>"
-            msg += f'\n<code>⏳ ETA</code>: {download.eta()}'
-            msg += f"\n<code>👤 User</code>: {download.message.from_user.mention} \n<code>🔗 User ID</code>: <spoiler>{download.message.from_user.id}</spoiler>"
+            msg += f"\n{progress_bar(download.progress())} » {download.progress()}"
+            msg += f"\n<code>Done</code>: {download.processed_bytes()} of {download.size()}"
+            msg += f"\n<code>Speed</code>: {download.speed()}"
+            msg += f"\n<code>Engine</code>: <b>{download.engine}</b>"
+            msg += f'\n<code>ETA</code>: {download.eta()}'
+            msg += f"\n<code>User</code>: {download.message.from_user.mention} \n<code>User ID</code>: <spoiler>{download.message.from_user.id}</spoiler>"
             if hasattr(download, 'seeders_num'):
                 with contextlib.suppress(Exception):
                     msg += f"\n<code>🌱 S/L</code>: {download.seeders_num()}/{download.leechers_num()}"
         elif download.status() == MirrorStatus.STATUS_SEEDING:
-            msg += f"\n<code>💽 Size</code>: {download.size()}"
-            msg += f"\n<code>🚀 Speed</code>: {download.upload_speed()}"
-            msg += f"\n<code>📈 Uploaded</code>: {download.uploaded_bytes()}"
-            msg += f"\n<code>📟 Ratio</code>: {download.ratio()}"
-            msg += f"\n<code>⏳ Time</code>: {download.seeding_time()}"
+            msg += f"\n<code>Size</code>: {download.size()}"
+            msg += f"\n<code>Speed</code>: {download.upload_speed()}"
+            msg += f"\n<code>Uploaded</code>: {download.uploaded_bytes()}"
+            msg += f"\n<code>Ratio</code>: {download.ratio()}"
+            msg += f"\n<code>Time</code>: {download.seeding_time()}"
         else:
-            msg += f"\n<code>💽 Size</code>: {download.size()}"
-        msg += f"\n<code>💯 Past</code>: {get_readable_time(time() - download.message.date.timestamp())}</blockquote>"
-        msg += f"\n<b> ❌⚠️: /stop_{download.gid()[:8]}</b>\n\n"
+            msg += f"\n<code>Size</code>: {download.size()}"
+        msg += f"\n<code>Past</code>: {get_readable_time(time() - download.message.date.timestamp())}</blockquote>"
+        msg += f"\n<b>⚠️: /stop_{download.gid()[:8]}</b>\n\n"
     if len(msg) == 0:
         return None, None
+    dl_speed = 0
+    up_speed = 0
+    for download in download_dict.values():
+            tstatus = download.status()
+            if tstatus == MirrorStatus.STATUS_DOWNLOADING:
+                spd = download.speed()
+                if 'K' in spd:
+                    dl_speed += float(spd.split('K')[0]) * 1024
+                elif 'M' in spd:
+                    dl_speed += float(spd.split('M')[0]) * 1048576
+            elif tstatus == MirrorStatus.STATUS_UPLOADING:
+                spd = download.speed()
+                if 'K' in spd:
+                    up_speed += float(spd.split('K')[0]) * 1024
+                elif 'M' in spd:
+                    up_speed += float(spd.split('M')[0]) * 1048576
+            elif tstatus == MirrorStatus.STATUS_SEEDING:
+                spd = download.upload_speed()
+                if 'K' in spd:
+                    up_speed += float(spd.split('K')[0]) * 1024
+                elif 'M' in spd:
+                    up_speed += float(spd.split('M')[0]) * 1048576
     if tasks > STATUS_LIMIT:
         buttons = ButtonMaker()
         buttons.callback("Prev", "status pre")
         buttons.callback(f"{PAGE_NO}/{PAGES}", "status ref")
         buttons.callback("Next", "status nex")
         button = buttons.column(3)
-    msg += f"<b>• Tasks</b>: {tasks}{bmax_task}"
-    msg += f"\n<b>• Bot uptime</b>: {current_time}"
-    msg += f"\n<b>• Free disk space</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
+    msg += f"<u><b>TASKS</b>: {tasks}{bmax_task}</u>"
+    msg += f"\n• <b>BotUP</b>: {current_time} | "
+    msg += f"<b>Free</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
+    msg += f"\n• <b>DL</b>: {get_readable_file_size(dl_speed)}/s | "
+    msg += f"<b>UL</b>: {get_readable_file_size(up_speed)}/s"
     return msg, button
-
 
 def text_to_bytes(size_text):
     size_text = size_text.lower()
