12c0af31df61b930b97b84d4e249ceb6d6df2f0c
LazaaR <Githeroku@outlook.com>
2025-03-11 10:07:25 +0530

    Update update.py

--- update.py
+++ update.py
@@ -81,7 +81,7 @@ if DATABASE_URL:
 
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = "https://github.com/LazaaR9/Aeon-Colab.git"
+    UPSTREAM_REPO = "https://github.com/LazaaR9/Aeon-Colab"
 
 UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
 if len(UPSTREAM_BRANCH) == 0:
